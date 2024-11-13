c4043985d7309d31cfc04bde286fc5516cffffb7
Steffen Kämmerer <kaemmi84@gmx.de>
2024-11-14 00:03:12 +0100

    Initial Commit

---
+++ CoppersignedWallPlacerTests/CoppersignedWallPlacerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoppersignedWallPlacerTests.swift
+//  CoppersignedWallPlacerTests
+//
+//  Created by Steffen Kämmerer on 14.11.24.
+//
+
+import Testing
+@testable import CoppersignedWallPlacer
+
+struct CoppersignedWallPlacerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
